803fb270f1400e4dc989e7107112560f4ac65c09
mjawadhussain40@gmail.com <mjawadhussain40@gmail.com>
2025-06-13 04:28:33 +0530

    changed build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # build.sh
+
 pip install -r requirements.txt
+python manage.py tailwind install
+python manage.py tailwind build
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
